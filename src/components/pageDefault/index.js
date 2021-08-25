36b9bf9a684b98620a0ab559b50df712fb8b4389
vianarafael <vianarafaelds@gmail.com>
2021-08-25 13:48:52 +0900

    updating the color export in other components

--- src/components/pageDefault/index.js
+++ src/components/pageDefault/index.js
@@ -3,21 +3,27 @@ import Menu from "../Menu";
 import Footer from "../Footer";
 
 import styled from "styled-components";
+import get from "lodash/get";
 
 const Main = styled.main`
-  background-color: var(--black);
-  color: var(--white);
   flex: 1;
   padding-top: 50px;
-  /* padding-left: 5%; */
   padding-right: 5%;
+
+  ${({ variant, theme }) => {
+    return `background-color: ${get(theme, `colors.${variant}.background`)};`;
+  }}
+
+  ${({ variant, theme }) => {
+    return `color: ${get(theme, `colors.${variant}.contrastText`)};`;
+  }}
 `;
 
 export default function PageDefault({ children, currentUser }) {
   return (
     <>
       <Menu />
-      <Main>{children}</Main>
+      <Main variant="primary">{children}</Main>
       <Footer />
     </>
   );
--- src/theme/index.js
+++ src/theme/index.js
@@ -9,3 +9,14 @@ const colors = {
 export default {
   colors,
 };
+
+//      color reference
+//   --primary: #2a7ae4;
+//   --black: #000000;
+//   --blackLighter: #9e9e9e;
+//   --grayLight: #f5f5f5;
+//   --grayMedium: #e5e5e5;
+//   --white: #ffffff;
+//   --frontEnd: #6bd1ff;
+//   --backEnd: #00c86f;
+//
