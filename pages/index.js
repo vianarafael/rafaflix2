e8058a0b7a65c1547fb9a703c788278026f27787
Rafael Viana <vianarafaelds@gmail.com>
2021-08-29 14:37:35 +0900

    (WIP) fetched movie data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
 import PageDefault from "../src/components/commons/pageDefault";
 import Text from "../src/foundation/Text";
 
-export default function Home() {
+export default function Home({ movieData }) {
+  console.log("movie ddata ", movieData.results);
   return (
     <PageDefault>
       <Text
@@ -15,4 +15,18 @@ export default function Home() {
       </Text>
     </PageDefault>
   );
+}
+
+export async function getStaticProps() {
+  // const movieData = await axios.get(
+  //   `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.filmAppKey}&language=en-US&page=1`
+  // );
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.filmAppKey}&language=en-US&page=1`
+  );
+  const movieData = await res.json();
+
+  return {
+    props: { movieData }, // will be passed to the page component as props
+  };
 }
