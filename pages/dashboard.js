7c61557bd6a757c58bd5e5f8f81dc18dbac7e189
Rafael Viana <vianarafaelds@gmail.com>
2021-09-19 13:51:11 +0900

    (WIP) finished showing single movie on dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -12,23 +12,50 @@ import { collection, getDocs, getFirestore } from "firebase/firestore";
 // 3. Chat Room
 // The user can enter a chat room and connect with other users
 
-export default function Dashboard() {
+export default function Dashboard({ films }) {
   const [user, setUser] = React.useState("");
+
   auth.onAuthStateChanged((user) => {
     setUser(user);
   });
+  console.log(films);
 
-  React.useEffect(async () => {
-    const db = getFirestore();
-    const querySnapshot = await getDocs(collection(db, "movies"));
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
-  }, []);
   return (
     <PageDefault>
       <h3>{`${user.displayName}'s Movies`}</h3>
+      <ul>
+        {films.map((film) => {
+          return (
+            <>
+              <h4>{film.title}</h4>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${film.backdrop_path}`}
+              />
+            </>
+          );
+        })}
+      </ul>
     </PageDefault>
   );
+}
+
+export async function getStaticProps({ params }) {
+  let id;
+  const db = getFirestore();
+  const querySnapshot = await getDocs(collection(db, "movies"));
+  console.log(querySnapshot);
+  querySnapshot.forEach(async (doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    id = doc.data().id;
+  });
+  const films = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.filmAppKey}&append_to_response=videos,credits`
+  ).then((res) => res.json());
+
+  console.log(films);
+  // just a single movie -this needs to be several movies
+
+  return {
+    props: { films: [films] },
+  };
 }
